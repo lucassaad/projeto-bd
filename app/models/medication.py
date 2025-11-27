0a29b459017729a4caadb038d873fb852889aa23
Lucas Saad Rodrigues <lucassaadro@gmail.com>
2025-11-27 17:55:43 -0300

    feat: add model for medication

---
+++ app/models/medication.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String
+from sqlalchemy.orm import mapped_column, Mapped
+
+from app.models.registry import table_registry
+
+
+@table_registry.mapped_as_dataclass
+class Medication:
+    __tablename__ = "medication"
+
+    anvisa_code: Mapped[str] = mapped_column(
+        String(13), primary_key=True
+    )
+    name: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(String(200))
