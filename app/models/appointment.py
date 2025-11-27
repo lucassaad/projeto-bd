318582c7988d171476be51176956d9560b890486
Lucas Saad Rodrigues <lucassaadro@gmail.com>
2025-11-27 17:31:19 -0300

    feat: add appointment model

---
+++ app/models/appointment.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+from app.models.doctor import Doctor
+from app.models.patient import Patient
+from app.models.ubs import Ubs
+from app.models.registry import table_registry
+
+
+@table_registry.mapped_as_dataclass
+class Appointment:
+    __tablename__ = "appointment"
+
+    id: Mapped[int] = mapped_column(
+        init=False, primary_key=True, autoincrement=True
+    )
+    doctor_cpf: Mapped[str] = mapped_column(
+        String(11), ForeignKey("doctor.cpf")
+    )
+    patient_cpf: Mapped[str] = mapped_column(
+        String(11), ForeignKey("patient.cpf")
+    )
+    ubs_cnes: Mapped[str] = mapped_column(
+        String(7), ForeignKey("ubs.cnes")
+    )
+
+    doctor: Mapped[Doctor] = relationship("Doctor")
+    patient: Mapped[Patient] = relationship("Patient")
+    ubs: Mapped[Ubs] = relationship("Ubs")
+
+    appointment_datetime: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.now
+    )
