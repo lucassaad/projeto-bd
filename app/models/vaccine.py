acc471e3394342666eadc79cae6e9fb07154abfe
Lucas Saad Rodrigues <lucassaadro@gmail.com>
2025-11-28 10:38:53 -0300

    feat: add model for vaccine

---
+++ app/models/vaccine.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.registry import table_registry
+from app.models.patient import Patient
+from app.models.nurse import Nurse
+
+
+@table_registry.mapped_as_dataclass
+class Vaccine:
+    __tablename__ = "vaccine"
+
+    id: Mapped[int] = mapped_column(
+        init=False, primary_key=True, autoincrement=True
+    )
+    patient_cpf: Mapped[int] = mapped_column(
+        String(11), ForeignKey("patient.cpf")
+    )
+    nurse_cpf: Mapped[str] = mapped_column(
+        String(11), ForeignKey('nurse.cpf')
+    )
+    name: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(String(200))
+    manufacturer: Mapped[str] = mapped_column(String(100))
+
+    patient: Mapped[Patient] = relationship("Patient")
+    nurse: Mapped[Nurse] = relationship("Nurse")
