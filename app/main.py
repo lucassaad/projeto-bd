5e9f9feb5faf898afebf7e1732ee6ae046bbab72
Lucas Saad Rodrigues <lucassaadro@gmail.com>
2025-11-30 20:50:20 -0300

    feat: add basic CRUD for Specialty

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 from app.api import user
 from app.api import appointment
 from app.api import ubs
+from app.api import specialty
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(appointment.router)
 app.include_router(ubs.router)
+app.include_router(specialty.router)
 
 
 @app.get('/')
