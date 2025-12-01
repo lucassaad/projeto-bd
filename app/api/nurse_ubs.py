0103afe50171866baa1e0d815d66e15bcf82d3e1
Lucca Schoen <schoenlucca@gmail.com>
2025-11-30 21:57:39 -0300

    finalização dos nurse_ubs

---
+++ app/api/nurse_ubs.py
@@ -0,0 +1,53 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.db.session import get_session
+from app.repositories.appointment import (
+    create_nurse_ubs,
+    delete_nurse_ubs_db,
+    select_all_nurse_ubss,
+    select_nurse_ubs,
+    update_nurse_ubs,
+)
+from app.schemas.nurse_ubs import Nurse_ubsIn, Nurse_ubsOut, Nurse_ubsUpdate
+
+router = APIRouter(prefix='/user', tags=['Nurse_ubs'])
+
+db_session = Annotated[Session, Depends(get_session)]
+
+@router.post('/', response_model=Nurse_ubsOut, status_code=HTTPStatus.CREATED)
+def post_nurse_ubs(nurse_ubs_in: Nurse_ubsIn, session: db_session):
+    nurse_ubs = create_nurse_ubs(nurse_ubs_in, session)
+    if nurse_ubs is None:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT, detail='Nurse_ubs already exists'
+        )
+    return nurse_ubs
+
+@router.get('/', response_model=Nurse_ubsOut, status_code=HTTPStatus.OK)
+def get_nurse_ubs(nurse_cpf: str, ubs_cnes: str, db_session: db_session):
+    nurse_ubs = select_nurse_ubs(nurse_cpf, ubs_cnes, db_session)
+    if nurse_ubs is None:
+        raise HTTPException(status_code=404, detail='Nurse_ubs not found')
+    return nurse_ubs
+
+@router.get('/all', response_model=list[Nurse_ubsOut], status_code=HTTPStatus.OK)
+def get_all_nurse_ubs(db_session: db_session):
+    return select_all_nurse_ubss(db_session)
+
+@router.put('/{id}', response_model=Nurse_ubsOut, status_code=HTTPStatus.OK)
+def put_nurse_ubs(id: int, nurse_ubs_update: Nurse_ubsUpdate, db_session: db_session):
+    nurse_ubs = update_nurse_ubs(nurse_ubs_update, id, db_session)
+    if nurse_ubs is None:
+        raise HTTPException(status_code=404, detail='Nurse_ubs not found')
+    return nurse_ubs
+
+@router.delete('/{id}', response_model=Nurse_ubsOut, status_code=HTTPStatus.OK)
+def delete_nurse_ubs(id: int, db_session: db_session):
+    nurse_ubs = delete_nurse_ubs_db(id, db_session)
+    if nurse_ubs is None:
+        raise HTTPException(status_code=404, detail='Nurse_ubs not found')
+    return nurse_ubs
