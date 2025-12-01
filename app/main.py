d11308b4aa0c70928aa77722a8edef5da44b3c59
Lucas Saad Rodrigues <lucassaadro@gmail.com>
2025-11-30 21:28:11 -0300

    feat: add basic CRUD for vaccine

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.api import user
 from app.api import appointment
 from app.api import ubs
 from app.api import specialty
+from app.api import vaccine
 
 app = FastAPI()
 
@@ -11,7 +12,7 @@ app.include_router(user.router)
 app.include_router(appointment.router)
 app.include_router(ubs.router)
 app.include_router(specialty.router)
-
+app.include_router(vaccine.router)
 
 @app.get('/')
 def root():
